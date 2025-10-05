c3a6d24388085488959580abe36e3a8c08c09cdc
Arsali18 <92elarsali@gmail.com>
2025-10-05 22:32:09 +0700

    Initial commit

---
+++ src/test/java/org/example/TestCase.java
@@ -0,0 +1,92 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestCase {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        // Pastikan ChromeDriver ada di PATH kamu
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void endToEndTest() throws InterruptedException {
+        //Driver SetUp
+        driver.get("https://www.saucedemo.com/");
+
+        //Input field Username
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        usernameField.sendKeys("standard_user");
+
+        //Input field password
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        passwordField.sendKeys("secret_sauce");
+
+        //Klik button Login
+        WebElement loginButton = driver.findElement(By.className("submit-button"));
+        loginButton.click();
+
+        Thread.sleep(4000); // jeda selama 4 detik
+
+        WebElement label = driver.findElement(By.xpath("//span[@class='title']"));
+        System.out.println("Title halaman: " + label);
+
+        Thread.sleep(2000); // jeda selama 3 detik
+
+        WebElement cadtButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        cadtButton.click();
+
+        WebElement cartButton = driver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
+        cartButton.click();
+
+        Thread.sleep(2000); // jeda selama 2 detik
+
+
+        //Klik button checkout
+        WebElement checkoutBTN = driver.findElement(By.name("checkout"));
+        checkoutBTN.click();
+
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        firstName.sendKeys("Arsali");
+        WebElement lastName = driver.findElement(By.xpath("//input[@id='last-name']"));
+        lastName.sendKeys("Ali");
+        WebElement zipField = driver.findElement(By.xpath("//input[@id='postal-code']"));
+        zipField.sendKeys("Bojong belung");
+
+        //Klik button Continue
+        WebElement continueButton = driver.findElement(By.xpath("//input[@id='continue']"));
+        continueButton.click();
+
+        //Klik button finish
+        WebElement finishButton = driver.findElement(By.xpath("//button[@id='finish']"));
+        finishButton.click();
+
+        Thread.sleep(4000); // jeda selama 4 detik
+        }
+
+    @AfterClass
+    public void tearDown () {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    }
+
+
+
+
+
